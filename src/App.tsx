816b7ac9a96a346bac8f0f3c4589265c288694a4
SeokJeong0411 <dk04188@naver.com>
2023-11-29 22:53:47 +0900

    init routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,71 @@
-import styled, { keyframes } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700&display=swap');
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
   }
-  100% {
-    transform: rotate(720deg);
-    border-radius: 0px
+  body {
+    line-height: 1;
   }
-`;
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
 
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 48px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  *{
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Gothic A1', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor}
+  }
+  a {
+    text-decoration: none;
   }
 `;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>asdf</Title>
-      <Box>
-        <Emoji>👩🏻</Emoji>
-      </Box>
-    </Wrapper>
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,13 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+export const theme: DefaultTheme = {
+  bgColor: "#2d3436",
+  textColor: "#dfe6e9",
+  accentColor: "#55efc4",
 };
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
-};
+// export const darkTheme: DefaultTheme = {
+//   bgColor: "black",
+//   textColor: "white",
+//   btnColor: "teal",
+// };
