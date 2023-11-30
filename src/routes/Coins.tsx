3e5929d8119ccc5fd83be07c2d5e6bddcafc8987
SeokJeong0411 <dk04188@naver.com>
2023-11-30 23:36:34 +0900

    20231130 init coin.tsx

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -26,13 +26,14 @@ const CoinsList = styled.ul``;
 const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.bgColor};
-  padding: 20px;
   border-radius: 15px;
   margin-bottom: 10px;
 
   a {
     transition: color 0.2s ease-in-out;
-    display: block;
+    display: flex;
+    align-items: center;
+    padding: 20px;
   }
 
   &:hover {
@@ -45,6 +46,12 @@ const Coin = styled.li`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
+`;
+
+const CoinIcon = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -81,7 +88,15 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <CoinIcon src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
